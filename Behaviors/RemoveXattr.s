8f74227c77b51011b695f8b466e37a5a2127b8ab
l0psec <67293122+l0psec@users.noreply.github.com>
2023-10-19 11:21:48 -0400

    Update RemoveXattr.s

--- Behaviors/RemoveXattr.s
+++ Behaviors/RemoveXattr.s
@@ -1,4 +1,6 @@
 // This uses the removexattr syscall to remove an extended attribute of a target file.  
+//Mitre - T1553.001
+//ES Event Type - es_event_deleteextattr_t
 
 .global _start
 .align 2
@@ -7,12 +9,12 @@ _start:
         adr     x1, att     // loading the ascii string in att label as name parameter
         mov     x2, #0      // moving 0 value for int option parameter
         mov     x16, #0xee  // moving syscall value in hex for removexattr into x16
-        svc     0x80        // supervisor call
+        svc     0xFFFF        // supervisor call
 
 exit:
         mov     x0, #0      // error code parameter for exit syscall
         mov     x16, #1     // exit syscall value
-        svc     0x80        // supervisor call
+        svc     0xFFFF       // supervisor call
 
 
 path:

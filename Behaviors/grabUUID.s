b728ce88c12fb1bdce411e79cd0857267e65418d
l0psec <67293122+l0psec@users.noreply.github.com>
2023-10-19 10:01:49 -0400

    Update grabUUID.s

--- Behaviors/grabUUID.s
+++ Behaviors/grabUUID.s
@@ -34,18 +34,18 @@ branch:                             		//branch label
     	strb   	w11, [x1], #0x1              	//store byte in x11 into x1 with an offset of x1
     	
 loadloop:
-    	ldrb   	w13, [x0, x8]			// load byte of address = x0 + x8 (at this point, x8 = 0 so this adds the first byte of the uuid value stored in x0)
-    	lsr    	x13, x13, #4			// here we perform a logical shift right for the value in x1 which is set to 1c. the result will be 1 and stored into x13, 1c >> 4 = 1
-    	ldrb   	w13, [x12, x13]			// load byte from the calculated address of x12 "123456789ABCDEF" + x13. x12 points to 0x1836af034 and x13 = 1 so we load the first byte of 0x1836af034 + 1
-    	strb   	w13, [x1]			// x13 now contains the first byte of the pointer in x12 which in this case is the value 0x31 which corresponds to the ascii value for 1. this is stored.
-    	ldrb   	w13, [x0, x8]			// a byte is loaded from the calculated address of x0 and x8 (0x16fdff578 + 0) = the uuid buffer.
-    	and    	x13, x13, #0xf			//1c stored in x13 is then and'd with f and for the first case results in C (12 d) into x13
-    	ldrb   	w13, [x12, x13]			//0x1836af034 + c = 0x1836af040 = "CDEF" so the C ascii character gets loaded into x13
-    	strb   	w13, [x1, #0x1]			//stores the C ascii value (0x43) into the memory location calculation of x1 (0x16fdff553) and 0x1 = 0x16fdff554: "C"
-    	add    	x1, x1, #0x2			//adds the hex value 0x2 to x1 and stores the result back into x1. so x1 will now point to 0x16fdff555 which will be used to add additional values.
+    	ldrb   	w13, [x0, x8]			// load byte of address = x0 + x8 (the first time x8 = 0 so this adds the first byte of the uuid value stored in x0)
+    	lsr    	x13, x13, #4			// here we perform a logical shift right for the value in x13
+    	ldrb   	w13, [x12, x13]			// load byte from the calculated address of x12 "123456789ABCDEF"
+    	strb   	w13, [x1]			// x13 now contains the first byte of the pointer in x12 and is stored in the memory address of x1
+    	ldrb   	w13, [x0, x8]			// a byte is loaded from the calculated address of x0 and x8 = the uuid buffer.
+    	and    	x13, x13, #0xf			//value stored in x13 is then and'd with 0xf
+    	ldrb   	w13, [x12, x13]			//ascii character gets loaded into x13
+    	strb   	w13, [x1, #0x1]			//stores the ascii value into the memory location calculation of x1
+    	add    	x1, x1, #0x2			//adds the hex value 0x2 to x1 and stores the result back into x1.
     	add    	x8, x8, #0x1			//0x1 is added to our counter to loop through all 16 bytes
-    	cmp    	w8, #0x10			//compares the value in w8 (which for the first loop will be 0x1) against 0x10 (16 d) and branches to 0x18363e280 to continue looping through bytes.
-    	b.ne   	branch
+    	cmp    	w8, #0x10			//compares the value in w8 (which for the first loop will be 0x1) against 0x10 (16 d).
+    	b.ne   	branch				//branches to the branch label to continue looping through bytes if w8 was not equal to 0x10
     	strb   	wzr, [x1]
 
 //TODO add dashes

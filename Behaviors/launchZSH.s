e4eec787895739ba110b2a417c6197dcadaafd95
l0psec <67293122+l0psec@users.noreply.github.com>
2023-10-19 10:04:38 -0400

    Create launchZSH.s

---
+++ Behaviors/launchZSH.s
@@ -0,0 +1,21 @@
+//launch a zsh shell
+.global _start
+.align 4
+_start:
+
+    mov     x3, #0x622F              //move "/bin/zsh" into x3 "2F 62 69 6E 2F 7A 73 68"
+    movk    x3, #0x6E69, lsl#16
+    movk    x3, #0x7A2F, lsl#32
+    movk    x3, #0x6873, lsl#48
+    stp     x3, xzr, [sp, #-16]!
+    add     x0, sp, xzr
+    stp     x0, xzr, [sp, #-16]!
+    add     x1, sp, xzr
+    mov     x2, xzr
+    mov     x16, #59
+    svc     0xFFFF
+
+exit:
+    mov     x0, #0      // error code parameter for exit syscall
+    mov     x16, #1     // exit syscall value
+    svc     0xFFF        // supervisor call

78ecb0a7675c1c6327c38b2a3310a0de6b550d5c
l0psec <67293122+l0psec@users.noreply.github.com>
2023-10-19 16:02:47 -0400

    Update launchZSH.s

--- Behaviors/launchZSH.s
+++ Behaviors/launchZSH.s
@@ -7,16 +7,16 @@
 .align 4
 _start:
 
-    mov     x3, #0x622F              //move "/bin/zsh" into x3 "2F 62 69 6E 2F 7A 73 68"
-    movk    x3, #0x6E69, lsl#16
+    mov     x3, #0x622F                  //move "/bin/zsh" into x3 "2F 62 69 6E 2F 7A 73 68"
+    movk    x3, #0x6E69, lsl#16          //multiple movk instructions are used to add all ascii values to x3  
     movk    x3, #0x7A2F, lsl#32
     movk    x3, #0x6873, lsl#48
     stp     x3, xzr, [sp, #-16]!
     add     x0, sp, xzr
     stp     x0, xzr, [sp, #-16]!
     add     x1, sp, xzr
-    mov     x2, xzr
-    mov     x16, #59
+    mov     x2, xzr                     //move 0 register into x2   
+    mov     x16, #59                    //execve syscall
     svc     0xFFFF
 
 exit:
